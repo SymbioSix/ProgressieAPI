4a41b941f6287631c657102899ae3fab0580bfe9
krisnaganesha1609 <igkganes@gmail.com>
2024-07-15 18:56:19 +0700

    feat: added ConnectViaAPI

--- setup/setup.go
+++ setup/setup.go
@@ -3,13 +3,18 @@ package setup
 import (
 	"log"
 
+	api "github.com/SymbioSix/ProgressieAPI/utils"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
-var DB *gorm.DB
+var (
+	DB *gorm.DB
 
-func ConnectDatabase(config *Config) {
+	Client *api.Client
+)
+
+func ConnectDatabase(config *Config) error {
 	var err error
 	dbName := config.DBName
 	dbHost := config.DBHost
@@ -25,4 +30,20 @@ func ConnectDatabase(config *Config) {
 	log.Println("Connected to Database!")
 
 	DB = database
+	return err
+}
+
+func ConnectViaAPI(config *Config) {
+	apiKey := config.APIKey
+	apiRef := config.APIRef
+
+	client, err := api.NewClient(apiRef, apiKey, &api.ClientOptions{})
+	if err != nil {
+		log.Fatal(err)
+		panic("Can't connect to API Gateway")
+	}
+
+	log.Println("Connected to API Gateway")
+
+	Client = client
 }
