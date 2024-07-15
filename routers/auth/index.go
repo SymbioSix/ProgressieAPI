256fa7a74a398b12df4757db97238a00d51c07d1
krisnaganesha1609 <igkganes@gmail.com>
2024-07-16 00:14:24 +0700

    new: init routers

---
+++ routers/auth/index.go
@@ -0,0 +1,24 @@
+package routers
+
+import (
+	"github.com/SymbioSix/ProgressieAPI/services/auth"
+	"github.com/gofiber/fiber/v3"
+)
+
+type AuthRouter struct {
+	authRouter auth.AuthController
+}
+
+func NewRouteAuthController(authRouter auth.AuthController) AuthRouter {
+	return AuthRouter{authRouter}
+}
+
+func (ar *AuthRouter) AuthRoutes(rg *fiber.Group) {
+	router := rg.Group("auth")
+
+	router.Post("/loginemailpassword", ar.authRouter.SignInWithEmailPassword)
+	router.Post("/signupemailpassword", ar.authRouter.SignUpWithEmailPassword)
+	router.Post("/logingoogle", ar.authRouter.SignInWithGoogle)
+	router.Post("/signupgoogle", ar.authRouter.SignUpWithGoogle)
+	router.Post("/signout", ar.authRouter.SignOut)
+}
