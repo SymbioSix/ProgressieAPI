14811bbba658569b7a19b51f4f4e2a7153dfe001
krisnaganesha1609 <igkganes@gmail.com>
2024-07-15 08:47:29 +0700

    feat: added setup database

--- setup/loadEnv.go
+++ setup/loadEnv.go
@@ -1,1 +1,31 @@
 package setup
+
+import (
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	DBHost     string `mapstructure:"SUPA_DATABASE_HOST"`
+	DBPort     string `mapstructure:"SUPA_DATABASE_PORT"`
+	DBName     string `mapstructure:"SUPA_DATABASE_NAME"`
+	DBUserName string `mapstructure:"SUPA_DATABASE_USER"`
+	DBPassword string `mapstructure:"SUPA_DATABASE_PASSWORD"`
+
+	ServerPort string `mapstructure:"SERVER_PORT"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigType("env")
+	viper.SetConfigName("")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
--- setup/setup.go
+++ setup/setup.go
@@ -1,1 +1,28 @@
 package setup
+
+import (
+	"log"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectDatabase(config *Config) {
+	var err error
+	dbName := config.DBName
+	dbHost := config.DBHost
+	dbPort := config.DBPort
+	dbUser := config.DBUserName
+	dbPass := config.DBPassword
+	database, err := gorm.Open(postgres.Open("user="+dbUser+" password="+dbPass+" host="+dbHost+" port="+dbPort+" dbname="+dbName), &gorm.Config{})
+	if err != nil {
+		log.Fatal(err)
+		panic("Can't connect to database!")
+	}
+
+	log.Println("Connected to Database!")
+
+	DB = database
+}
