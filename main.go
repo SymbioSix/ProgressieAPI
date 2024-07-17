75144ed0c1ab11b01c9a8272e674b085c77f6399
krisnaganesha1609 <igkganes@gmail.com>
2024-07-17 15:46:34 +0700

    feat: add routes

--- main.go
+++ main.go
@@ -3,15 +3,20 @@ package main
 import (
 	"log"
 
+	au_r "github.com/SymbioSix/ProgressieAPI/routers/auth"
+	au_s "github.com/SymbioSix/ProgressieAPI/services/auth"
 	s "github.com/SymbioSix/ProgressieAPI/setup"
 	"github.com/gofiber/fiber/v3"
 	"github.com/gofiber/fiber/v3/middleware/cors"
+	"github.com/gofiber/fiber/v3/middleware/healthcheck"
 )
 
 var (
 	app *fiber.App
 
 	// TODO: Create New Service Controller and Router Variables
+	AuthController au_s.AuthController
+	AuthRouter     au_r.AuthRouter
 )
 
 func init() {
@@ -25,6 +30,9 @@ func init() {
 	s.ConnectViaAPI(&config)
 
 	// TODO: Initialize Routers and Controllers
+
+	AuthController = au_s.NewAuthController(s.DB, s.Client)
+	AuthRouter = au_r.NewRouteAuthController(AuthController)
 
 	app = fiber.New()
 }
@@ -45,21 +53,26 @@ func main() {
 
 	router := app.Group("/v1")
 	router.Get("/healthcheck", func(c fiber.Ctx) error {
-		status := "healthy"
-		description := "API is Ready"
+		database_status := "ready"
+		supabase_api_status := "ready"
+		overall_status := "super healthy"
 		healthmap := fiber.Map{
-			status:      status,
-			description: description,
+			"database_status":     database_status,
+			"supabase_api_status": supabase_api_status,
+			"overall_status":      overall_status,
 		}
-		if s.DB.Error != nil || !s.Client.Rest.Ping() {
-			status = "unhealthy"
-			description = "API has some error"
+		if s.DB.Error != nil {
+			database_status = "error"
+			if !s.Client.Rest.Ping() {
+				supabase_api_status = "error"
+			}
 			return c.Status(fiber.StatusInternalServerError).JSON(healthmap)
 		}
 		return c.JSON(healthmap)
-	})
+	}, healthcheck.NewHealthChecker(healthcheck.Config{Probe: func(c fiber.Ctx) bool { return true }}))
 
 	// Connect all the routes
+	AuthRouter.AuthRoutes(router)
 
 	// Serve The API
 	app.Listen(config.ServerAddr)
