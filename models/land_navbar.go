d4465aba9ac743d1643bb640d09ca25adee94df3
jayll0 <145304774+jayll0@users.noreply.github.com>
2024-07-19 14:51:31 +0700

    Update land_navbar.go

--- models/land_navbar.go
+++ models/land_navbar.go
@@ -3,30 +3,27 @@ package models
 import "time"
 
 type land_navbar_request struct {
-    NavComponentID     int `json:"nav_component_id"`
-    NavComponentName   string `json:"nav_component_name"`
-    NavComponentGroup  int `json:"nav_component_group"`
-    NavComponentIcon   string `json:"nav_component_icon"`
-    Tooltip            string `json:"tooltip"`
-    Endpoint           string `json:"endpoint"`
-    CreatedBy          string `json:"created_by"`
-    CreatedAt          time.Time `json:"created_at"`
-    UpdatedBy          string `json:"updated_by"`
-    UpdatedAt          time.Time `json:"updated_at"`
+    NavComponentID    int       `gorm:"column:nav_component_id" json:"nav_component_id"`
+    NavComponentName  string    `gorm:"column:nav_component_name" json:"nav_component_name"`
+    NavComponentGroup int       `gorm:"column:nav_component_group" json:"nav_component_group"`
+    NavComponentIcon  string    `gorm:"column:nav_component_icon" json:"nav_component_icon"`
+    Tooltip           string    `gorm:"column:tooltip" json:"tooltip"`
+    Endpoint          string    `gorm:"column:endpoint" json:"endpoint"`
+    CreatedBy         string    `gorm:"column:created_by" json:"created_by"`
+    CreatedAt         time.Time `gorm:"column:created_at" json:"created_at"`
+    UpdatedBy         string    `gorm:"column:updated_by" json:"updated_by"`
+    UpdatedAt         time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
 
-
-
-
 type land_navbar_response struct {
-    NavComponentID     int `json:"nav_component_id"`
-    NavComponentName   string `json:"nav_component_name"`
-    NavComponentGroup  int `json:"nav_component_group"`
-    NavComponentIcon   string `json:"nav_component_icon"`
-    Tooltip            string `json:"tooltip"`
-    Endpoint           string `json:"endpoint"`
-    CreatedBy          string `json:"created_by"`
-    CreatedAt          time.Time `json:"created_at"`
-    UpdatedBy          string `json:"updated_by"`
-    UpdatedAt          time.Time `json:"updated_at"`
+    NavComponentID    int       `gorm:"column:nav_component_id" json:"nav_component_id"`
+    NavComponentName  string    `gorm:"column:nav_component_name" json:"nav_component_name"`
+    NavComponentGroup int       `gorm:"column:nav_component_group" json:"nav_component_group"`
+    NavComponentIcon  string    `gorm:"column:nav_component_icon" json:"nav_component_icon"`
+    Tooltip           string    `gorm:"column:tooltip" json:"tooltip"`
+    Endpoint          string    `gorm:"column:endpoint" json:"endpoint"`
+    CreatedBy         string    `gorm:"column:created_by" json:"created_by"`
+    CreatedAt         time.Time `gorm:"column:created_at" json:"created_at"`
+    UpdatedBy         string    `gorm:"column:updated_by" json:"updated_by"`
+    UpdatedAt         time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
