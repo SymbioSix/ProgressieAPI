5e646c847daae05c5e55b43395a9d170eb24f3f1
jayll0 <145304774+jayll0@users.noreply.github.com>
2024-07-19 14:49:26 +0700

    Update land_aboutus.go

--- models/land_aboutus.go
+++ models/land_aboutus.go
@@ -2,32 +2,30 @@ package models
 
 import "time"
 
-
-
 type land_aboutus_request struct {
-    AboutUsComponentID   int            `json:"aboutus_component_id"` // Primary key
-    AboutUsComponentName string         `json:"aboutus_component_name"`
-    AboutUsComponentJobdesc string      `json:"aboutus_component_jobdesc"`
-    AboutUsComponentPhoto string        `json:"aboutus_component_photo"`
-    Description          string         `json:"description"`
-    Tooltip              string         `json:"tooltip"`
-    Status               string			`json:"status"`
-    CreatedBy            string         `json:"created_by"`
-    CreatedAt            time.Time      `json:"created_at"`
-    UpdatedBy            string         `json:"updated_by"`
-    UpdatedAt            time.Time      `json:"updated_at"`
+    AboutUsComponentID      int       `gorm:"column:aboutus_component_id" json:"aboutus_component_id"` // Primary key
+    AboutUsComponentName    string    `gorm:"column:aboutus_component_name" json:"aboutus_component_name"`
+    AboutUsComponentJobdesc string    `gorm:"column:aboutus_component_jobdesc" json:"aboutus_component_jobdesc"`
+    AboutUsComponentPhoto   string    `gorm:"column:aboutus_component_photo" json:"aboutus_component_photo"`
+    Description             string    `gorm:"column:description" json:"description"`
+    Tooltip                 string    `gorm:"column:tooltip" json:"tooltip"`
+    Status                  string    `gorm:"column:status" json:"status"`
+    CreatedBy               string    `gorm:"column:created_by" json:"created_by"`
+    CreatedAt               time.Time `gorm:"column:created_at" json:"created_at"`
+    UpdatedBy               string    `gorm:"column:updated_by" json:"updated_by"`
+    UpdatedAt               time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
 
 type land_aboutus_response struct {
-    AboutUsComponentID   int            `json:"aboutus_component_id"`
-    AboutUsComponentName string         `json:"aboutus_component_name"`
-    AboutUsComponentJobdesc string      `json:"aboutus_component_jobdesc"`
-    AboutUsComponentPhoto string        `json:"aboutus_component_photo"`
-    Description          string         `json:"description"`
-    Tooltip              string         `json:"tooltip"`
-    Status               string 		`json:"status"`
-    CreatedBy            string         `json:"created_by"`
-    CreatedAt            time.Time      `json:"created_at"`
-    UpdatedBy            string         `json:"updated_by"`
-    UpdatedAt            time.Time      `json:"updated_at"`
+    AboutUsComponentID      int       `gorm:"column:aboutus_component_id" json:"aboutus_component_id"`
+    AboutUsComponentName    string    `gorm:"column:aboutus_component_name" json:"aboutus_component_name"`
+    AboutUsComponentJobdesc string    `gorm:"column:aboutus_component_jobdesc" json:"aboutus_component_jobdesc"`
+    AboutUsComponentPhoto   string    `gorm:"column:aboutus_component_photo" json:"aboutus_component_photo"`
+    Description             string    `gorm:"column:description" json:"description"`
+    Tooltip                 string    `gorm:"column:tooltip" json:"tooltip"`
+    Status                  string    `gorm:"column:status" json:"status"`
+    CreatedBy               string    `gorm:"column:created_by" json:"created_by"`
+    CreatedAt               time.Time `gorm:"column:created_at" json:"created_at"`
+    UpdatedBy               string    `gorm:"column:updated_by" json:"updated_by"`
+    UpdatedAt               time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
