cb414cc3d0e7f3ba4d771238d0334a3cd7f5bb90
jayll0 <145304774+jayll0@users.noreply.github.com>
2024-07-19 14:54:46 +0700

    Update land_footer.go

--- models/land_footer.go
+++ models/land_footer.go
@@ -3,27 +3,27 @@ package models
 import "time"
 
 type land_footer_request struct {
-    FooterComponentID    int       `json:"footer_component_id"`   // Primary key
-    FooterComponentName  string    `json:"footer_component_name"`
-    FooterComponentGroup int       `json:"footer_component_group"` // Foreign key
-    FooterComponentIcon  string    `json:"footer_component_icon"`
-    Tooltip              string    `json:"tooltip"`
-    Endpoint             string    `json:"endpoint"`
-    CreatedBy            string    `json:"created_by"`
-    CreatedAt            time.Time `json:"created_at"`
-    UpdatedBy            string    `json:"updated_by"`
-    UpdatedAt            time.Time `json:"updated_at"`
+    FooterComponentID    int       `gorm:"column:footer_component_id" json:"footer_component_id"`   // Primary key
+    FooterComponentName  string    `gorm:"column:footer_component_name" json:"footer_component_name"`
+    FooterComponentGroup int       `gorm:"column:footer_component_group" json:"footer_component_group"` // Foreign key
+    FooterComponentIcon  string    `gorm:"column:footer_component_icon" json:"footer_component_icon"`
+    Tooltip              string    `gorm:"column:tooltip" json:"tooltip"`
+    Endpoint             string    `gorm:"column:endpoint" json:"endpoint"`
+    CreatedBy            string    `gorm:"column:created_by" json:"created_by"`
+    CreatedAt            time.Time `gorm:"column:created_at" json:"created_at"`
+    UpdatedBy            string    `gorm:"column:updated_by" json:"updated_by"`
+    UpdatedAt            time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
 
 type land_footer_response struct {
-    FooterComponentID    int       `json:"footer_component_id"`
-    FooterComponentName  string    `json:"footer_component_name"`
-    FooterComponentGroup int       `json:"footer_component_group"`
-    FooterComponentIcon  string    `json:"footer_component_icon"`
-    Tooltip              string    `json:"tooltip"`
-    Endpoint             string    `json:"endpoint"`
-    CreatedBy            string    `json:"created_by"`
-    CreatedAt            time.Time `json:"created_at"`
-    UpdatedBy            string    `json:"updated_by"`
-    UpdatedAt            time.Time `json:"updated_at"`
+    FooterComponentID    int       `gorm:"column:footer_component_id" json:"footer_component_id"`
+    FooterComponentName  string    `gorm:"column:footer_component_name" json:"footer_component_name"`
+    FooterComponentGroup int       `gorm:"column:footer_component_group" json:"footer_component_group"`
+    FooterComponentIcon  string    `gorm:"column:footer_component_icon" json:"footer_component_icon"`
+    Tooltip              string    `gorm:"column:tooltip" json:"tooltip"`
+    Endpoint             string    `gorm:"column:endpoint" json:"endpoint"`
+    CreatedBy            string    `gorm:"column:created_by" json:"created_by"`
+    CreatedAt            time.Time `gorm:"column:created_at" json:"created_at"`
+    UpdatedBy            string    `gorm:"column:updated_by" json:"updated_by"`
+    UpdatedAt            time.Time `gorm:"column:updated_at" json:"updated_at"`
 }
