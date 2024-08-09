c3e15f4e565a2a120374c298532420c1ae269021
krisnaganesha1609 <igkganes@gmail.com>
2024-08-09 22:02:22 +0700

    feat: added UpdateUserProfile and UpdateUserProfileSkills

--- services/dashboard/index.go
+++ services/dashboard/index.go
@@ -3,6 +3,7 @@ package dashboard
 import (
 	auth "github.com/SymbioSix/ProgressieAPI/models/auth"
 	dashb "github.com/SymbioSix/ProgressieAPI/models/dashboard"
+	profile "github.com/SymbioSix/ProgressieAPI/models/profile"
 	status "github.com/SymbioSix/ProgressieAPI/models/status"
 	"github.com/SymbioSix/ProgressieAPI/utils"
 	"github.com/gofiber/fiber/v3"
@@ -32,7 +33,7 @@ func NewDashboardController(DB *gorm.DB, API *utils.Client) DashboardController 
 //	@Failure		401	{object}	status.StatusModel
 //	@Failure		500	{object}	status.StatusModel
 //	@Router			/dashboard/sidebar [get]
-func (dash *DashboardController) SidebarMapper(c fiber.Ctx) error {
+func (dash *DashboardController) SidebarMapperForAuthenticatedUser(c fiber.Ctx) error {
 	getUser, err := dash.API.Auth.GetUser()
 	if err != nil {
 		stat := status.StatusModel{Status: "fail", Message: err.Error()}
@@ -75,4 +76,115 @@ func (dash *DashboardController) GetUserProfile(c fiber.Ctx) error {
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"status": "fail", "message": getUserRole.Error.Error()})
 	}
 	return c.Status(fiber.StatusOK).JSON(userRoleResponse)
+}
+
+// UpdateUserProfile godoc
+//
+//	@Summary		Update user profile
+//	@Description	Update the profile of the authenticated user
+//	@Tags			Dashboard Service
+//	@Accept			json
+//	@Produce		json
+//	@Param			request	body		auth.UpdateUserProfileRequest	true	"Update User Profile Request"
+//	@Success		200		{object}	status.StatusModel
+//	@Failure		401		{object}	status.StatusModel
+//	@Failure		500		{object}	status.StatusModel
+//	@Router			/dashboard/profile [put]
+func (dash *DashboardController) UpdateUserProfile(c fiber.Ctx) error {
+	getUser, err := dash.API.Auth.GetUser()
+	if err != nil {
+		stat := status.StatusModel{Status: "fail", Message: err.Error()}
+		return c.Status(fiber.StatusUnauthorized).JSON(stat)
+	}
+
+	var request auth.UpdateUserProfileRequest
+	if err := c.Bind().JSON(&request); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
+	}
+
+	var updateUser auth.UserModel
+	if res := dash.DB.Where(auth.UserModel{UserID: getUser.ID}).First(&updateUser); res.Error != nil {
+		stat := status.StatusModel{Status: "fail", Message: res.Error.Error()}
+		return c.Status(fiber.StatusInternalServerError).JSON(stat)
+	}
+
+	if request.FirstName != "" {
+		updateUser.FirstName = request.FirstName
+	}
+	if request.LastName != "" {
+		updateUser.LastName = request.LastName
+	}
+	if request.Email != "" {
+		updateUser.Email = request.Email
+	}
+	if request.PhotoProfile != "" {
+		updateUser.PhotoProfile = request.PhotoProfile
+	}
+	if request.TitleProfile != "" {
+		updateUser.TitleProfile = request.TitleProfile
+	}
+	if request.PhoneNumber != "" {
+		updateUser.PhoneNumber = request.PhoneNumber
+	}
+	if request.Description != "" {
+		updateUser.Description = request.Description
+	}
+	if request.Gender != "" {
+		updateUser.Gender = request.Gender
+	}
+
+	if res := dash.DB.Save(&updateUser); res.Error != nil {
+		stat := status.StatusModel{Status: "fail", Message: res.Error.Error()}
+		return c.Status(fiber.StatusInternalServerError).JSON(stat)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "message": "User Profile Updated Successfully!"})
+}
+
+// UpdateUserSkill godoc
+//
+//	@Summary		Update user skill
+//	@Description	Update the skill of the authenticated user
+//	@Tags			Dashboard Service
+//	@Accept			json
+//	@Produce		json
+//	@Param			request	body		profile.UpdateUserTitleSkillRequest	true	"Update User Skill Request"
+//	@Success		200		{object}	status.StatusModel
+//	@Failure		401		{object}	status.StatusModel
+//	@Failure		500		{object}	status.StatusModel
+//	@Router			/dashboard/skill [put]
+func (dash *DashboardController) CreateOrUpdateUserSkill(c fiber.Ctx) error {
+	getUser, err := dash.API.Auth.GetUser()
+	if err != nil {
+		stat := status.StatusModel{Status: "fail", Message: err.Error()}
+		return c.Status(fiber.StatusUnauthorized).JSON(stat)
+	}
+
+	var request profile.UpdateUserTitleSkillRequest
+	if err := c.Bind().JSON(&request); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{"status": "fail", "message": err.Error()})
+	}
+
+	var updateUser profile.UserTitleSkill
+	if res := dash.DB.Where(auth.UserModel{UserID: getUser.ID}).First(&updateUser); res.Error != nil {
+		stat := status.StatusModel{Status: "fail", Message: res.Error.Error()}
+		return c.Status(fiber.StatusInternalServerError).JSON(stat)
+	}
+
+	if request.TitleSkill != "" {
+		updateUser.TitleSkill = request.TitleSkill
+	}
+	if request.Subtitle != "" {
+		updateUser.Subtitle = request.Subtitle
+	}
+	if request.UpdatedAt.String() != "" {
+		updateUser.UpdatedAt = request.UpdatedAt
+	}
+
+	if res := dash.DB.Save(&updateUser); res.Error != nil {
+		stat := status.StatusModel{Status: "fail", Message: res.Error.Error()}
+		return c.Status(fiber.StatusInternalServerError).JSON(stat)
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{"status": "success", "message": "User Skills Updated Successfully!"})
 }
