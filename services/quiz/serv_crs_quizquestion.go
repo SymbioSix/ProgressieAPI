ed22df1fe02073c3c5f553667651aef1a37fc81c
krisnaganesha1609 <igkganes@gmail.com>
2024-08-13 16:59:01 +0700

    fix: unnecessary pass by reference

--- services/quiz/serv_crs_quizquestion.go
+++ services/quiz/serv_crs_quizquestion.go
@@ -16,21 +16,21 @@ type QuizQuestionService struct {
 }
 
 // NewQuizQuestionService creates a new instance of QuizQuestionService
-func NewQuizQuestionService(db *gorm.DB) *QuizQuestionService {
-	return &QuizQuestionService{DB: db}
+func NewQuizQuestionService(db *gorm.DB) QuizQuestionService {
+	return QuizQuestionService{DB: db}
 }
 
 // CreateQuizQuestion creates a new QuizQuestion record
-// @Summary Create a new QuizQuestion
-// @Description Create a new QuizQuestion record
-// @Tags QuizQuestion
-// @Accept  json
-// @Produce  json
-// @Param quizQuestion body models.QuizQuestion true "QuizQuestion data"
-// @Success 201 {object} models.QuizQuestion
-// @Failure 400 {object} status.StatusModel
-// @Failure 500 {object} status.StatusModel
-// @Router /quiz-questions [post]
+//	@Summary		Create a new QuizQuestion
+//	@Description	Create a new QuizQuestion record
+//	@Tags			QuizQuestion Service
+//	@Accept			json
+//	@Produce		json
+//	@Param			quizQuestion	body		models.QuizQuestion	true	"QuizQuestion data"
+//	@Success		201				{object}	models.QuizQuestion
+//	@Failure		400				{object}	status.StatusModel
+//	@Failure		500				{object}	status.StatusModel
+//	@Router			/quiz-questions [post]
 func (s QuizQuestionService) CreateQuizQuestion(c fiber.Ctx) error {
 	var question models.QuizQuestion
 	if err := c.Bind().JSON(&question); err != nil {
@@ -51,15 +51,15 @@ func (s QuizQuestionService) CreateQuizQuestion(c fiber.Ctx) error {
 }
 
 // GetQuizQuestionByID retrieves a QuizQuestion record by ID
-// @Summary Get a QuizQuestion by ID
-// @Description Retrieve a QuizQuestion record by its ID
-// @Tags QuizQuestion
-// @Produce  json
-// @Param id path string true "QuizQuestion ID"
-// @Success 200 {object} models.QuizQuestion
-// @Failure 404 {object} status.StatusModel
-// @Failure 500 {object} status.StatusModel
-// @Router /quiz-questions/{id} [get]
+//	@Summary		Get a QuizQuestion by ID
+//	@Description	Retrieve a QuizQuestion record by its ID
+//	@Tags			QuizQuestion Service
+//	@Produce		json
+//	@Param			id	path		string	true	"QuizQuestion ID"
+//	@Success		200	{object}	models.QuizQuestion
+//	@Failure		404	{object}	status.StatusModel
+//	@Failure		500	{object}	status.StatusModel
+//	@Router			/quiz-questions/{id} [get]
 func (s QuizQuestionService) GetQuizQuestionByID(c fiber.Ctx) error {
 	id := c.Params("id")
 	var question models.QuizQuestion
@@ -79,17 +79,17 @@ func (s QuizQuestionService) GetQuizQuestionByID(c fiber.Ctx) error {
 }
 
 // UpdateQuizQuestion updates an existing QuizQuestion record
-// @Summary Update an existing QuizQuestion
-// @Description Update a QuizQuestion record by its ID
-// @Tags QuizQuestion
-// @Accept  json
-// @Produce  json
-// @Param id path string true "QuizQuestion ID"
-// @Param quizQuestion body models.QuizQuestion true "Updated QuizQuestion data"
-// @Success 200 {object} models.QuizQuestion
-// @Failure 400 {object} status.StatusModel
-// @Failure 500 {object} status.StatusModel
-// @Router /quiz-questions/{id} [put]
+//	@Summary		Update an existing QuizQuestion
+//	@Description	Update a QuizQuestion record by its ID
+//	@Tags			QuizQuestion Service
+//	@Accept			json
+//	@Produce		json
+//	@Param			id				path		string				true	"QuizQuestion ID"
+//	@Param			quizQuestion	body		models.QuizQuestion	true	"Updated QuizQuestion data"
+//	@Success		200				{object}	models.QuizQuestion
+//	@Failure		400				{object}	status.StatusModel
+//	@Failure		500				{object}	status.StatusModel
+//	@Router			/quiz-questions/{id} [put]
 func (s QuizQuestionService) UpdateQuizQuestion(c fiber.Ctx) error {
 	id := c.Params("id")
 	var question models.QuizQuestion
@@ -111,13 +111,13 @@ func (s QuizQuestionService) UpdateQuizQuestion(c fiber.Ctx) error {
 }
 
 // DeleteQuizQuestion deletes a QuizQuestion record by ID
-// @Summary Delete a QuizQuestion by ID
-// @Description Delete a QuizQuestion record by its ID
-// @Tags QuizQuestion
-// @Param id path string true "QuizQuestion ID"
-// @Success 204 "No Content"
-// @Failure 500 {object} status.StatusModel
-// @Router /quiz-questions/{id} [delete]
+//	@Summary		Delete a QuizQuestion by ID
+//	@Description	Delete a QuizQuestion record by its ID
+//	@Tags			QuizQuestion Service
+//	@Param			id	path	string	true	"QuizQuestion ID"
+//	@Success		204	"No Content"
+//	@Failure		500	{object}	status.StatusModel
+//	@Router			/quiz-questions/{id} [delete]
 func (s QuizQuestionService) DeleteQuizQuestion(c fiber.Ctx) error {
 	id := c.Params("id")
 	if err := s.DB.Where("quizquestion_id = ?", id).Delete(&models.QuizQuestion{}).Error; err != nil {
