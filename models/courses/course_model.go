07e72e1f9019de02ddad59f09b3d9ec1f9220d48
krisnaganesha1609 <igkganes@gmail.com>
2024-08-29 18:56:30 +0700

    fix: subcourse progress models

--- models/courses/course_model.go
+++ models/courses/course_model.go
@@ -4,6 +4,8 @@ import (
 	"time"
 
 	models "github.com/SymbioSix/ProgressieAPI/models/Todo"
+	quiz "github.com/SymbioSix/ProgressieAPI/models/quiz"
+	"github.com/google/uuid"
 )
 
 type CourseModel struct {
@@ -36,9 +38,10 @@ type SubCourseModel struct {
 	CreatedAt       time.Time                      `gorm:"column:created_at" json:"created_at"`
 	UpdatedBy       string                         `gorm:"column:updated_by" json:"updated_by,omitempty"`
 	UpdatedAt       time.Time                      `gorm:"column:updated_at" json:"updated_at,omitempty"`
-	Progress        models.SubcourseProgress       `gorm:"foreignKey:SubcourseID;references:SubcourseID" json:"progress,omitempty"`
+	Progress        SubcourseProgress              `gorm:"foreignKey:SubcourseID;references:SubcourseID" json:"progress,omitempty"`
 	VideoContent    SubCourseVideoContentModel     `gorm:"foreignKey:SubcourseID;references:SubcourseID" json:"video_content,omitempty"`
 	ReadingContents []SubCourseReadingContentModel `gorm:"foreignKey:SubcourseID;references:SubcourseID" json:"reading_contents,omitempty"`
+	Quizzes         []quiz.Quiz                    `gorm:"foreignKey:SubcourseID;references:SubcourseID" json:"quizzes,omitempty"`
 }
 
 func (crs *SubCourseModel) TableName() string {
@@ -46,11 +49,14 @@ func (crs *SubCourseModel) TableName() string {
 }
 
 type SubCourseVideoContentModel struct {
-	SubcourseID string `gorm:"column:subcourse_id;primaryKey" json:"subcourse_id"`
-	VideoLink   string `gorm:"column:video_link" json:"video_link"`
-	Description string `gorm:"column:description" json:"description"`
-	Status      string `gorm:"column:status" json:"status"`
-	EarnedPoint int    `gorm:"column:earned_point" json:"earned_point"`
+	SubcourseID    string `gorm:"column:subcourse_id;primaryKey" json:"subcourse_id"`
+	VideoTitle     string `gorm:"column:video_title" json:"video_title"`
+	VideoLink      string `gorm:"column:video_link" json:"video_link"`
+	Description    string `gorm:"column:description" json:"description"`
+	Status         string `gorm:"column:status" json:"status"`
+	EarnedPoint    int    `gorm:"column:earned_point" json:"earned_point"`
+	Source         string `gorm:"column:source" json:"source"`
+	PlatformSource string `gorm:"column:platform_source" json:"platform_source"`
 }
 
 func (crs *SubCourseVideoContentModel) TableName() string {
@@ -84,4 +90,33 @@ type SubCourseReadingImageContentModel struct {
 
 func (crs *SubCourseReadingImageContentModel) TableName() string {
 	return "crs_subcoursereadingimage"
+}
+
+type SubcourseProgress struct {
+	SubcourseprogressID uuid.UUID                    `gorm:"column:subcourseprogress_id;primaryKey" json:"subcourseprogress_id"`
+	UserID              uuid.UUID                    `gorm:"column:user_id" json:"user_id"`
+	SubcourseID         string                       `gorm:"column:subcourse_id" json:"subcourse_id"`
+	IsVideoViewed       bool                         `gorm:"column:is_video_viewed" json:"is_video_viewed"`
+	IsSubcourseFinished bool                         `gorm:"column:is_subcourse_finished" json:"is_subcourse_finished"`
+	CreatedBy           string                       `gorm:"column:created_by" json:"created_by"`
+	CreatedAt           time.Time                    `gorm:"column:created_at" json:"created_at"`
+	UpdatedBy           string                       `gorm:"column:updated_by" json:"updated_by"`
+	UpdatedAt           time.Time                    `gorm:"column:updated_at" json:"updated_at"`
+	ReadingProgress     []SubcourseProgressReading   `gorm:"foreignKey:SubcourseprogressID;references:SubcourseprogressID" json:"reading_progress,omitempty"`
+	Reminders           []models.TdSubcourseReminder `gorm:"foreignKey:SubcourseprogressID;references:SubcourseprogressID" json:"reminders,omitempty"`
+}
+
+func (td *SubcourseProgress) TableName() string {
+	return "crs_subcourseprogress"
+}
+
+type SubcourseProgressReading struct {
+	SubcourseprogressreadingID uuid.UUID `gorm:"column:subcourseprogressreading_id;primaryKey" json:"subcourseprogressreading_id"`
+	SubcourseprogressID        uuid.UUID `gorm:"column:subcourseprogress_id" json:"subcourseprogress_id"`
+	SubcoursereadingID         string    `gorm:"column:subcoursereading_id" json:"subcoursereading_id"`
+	IsReaded                   bool      `gorm:"column:is_readed" json:"is_readed"`
+}
+
+func (td *SubcourseProgressReading) TableName() string {
+	return "crs_subcourseprogressreading"
 }
