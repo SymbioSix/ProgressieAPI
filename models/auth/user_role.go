b44c88886d72e4cd86560f33d1cef5610fba8e07
krisnaganesha1609 <igkganes@gmail.com>
2024-09-04 12:54:35 +0700

    fix: now usermodel receive subcourseprogress and enrollmentprogress

--- models/auth/user_role.go
+++ models/auth/user_role.go
@@ -3,33 +3,38 @@ package models
 import (
 	"time"
 
+	course "github.com/SymbioSix/ProgressieAPI/models/courses"
 	models "github.com/SymbioSix/ProgressieAPI/models/profile"
+	quiz "github.com/SymbioSix/ProgressieAPI/models/quiz"
 	"github.com/google/uuid"
 )
 
 type UserModel struct {
-	UserID                  uuid.UUID                `gorm:"column:user_id" json:"id,omitempty"`
-	FirstName               string                   `gorm:"column:first_name" json:"first_name,omitempty"`
-	LastName                string                   `gorm:"column:last_name" json:"last_name,omitempty"`
-	Email                   string                   `gorm:"column:email" json:"email"`
-	PhoneNumber             string                   `gorm:"column:phone_number" json:"phone_number,omitempty"`
-	PhotoProfile            string                   `gorm:"column:photo_profile_link" json:"photo_profile_link,omitempty"`
-	TitleProfile            string                   `gorm:"column:title_desc_profile" json:"title_desc_profile,omitempty"`
-	Description             string                   `gorm:"column:description" json:"description,omitempty"`
-	EmailVerified           bool                     `gorm:"column:email_verified" json:"email_verified,omitempty"`
-	PhoneVerified           bool                     `gorm:"column:phone_verified" json:"phone_verified,omitempty"`
-	Username                string                   `gorm:"column:username" json:"username"`
-	Gender                  string                   `gorm:"column:gender" json:"gender,omitempty"`
-	TotalCoursesFinished    int                      `gorm:"column:total_courses_finished" json:"total_courses_finished,omitempty"`
-	TotalSubcoursesFinished int                      `gorm:"column:total_subcourses_finished" json:"total_subcourses_finished,omitempty"`
-	TotalPointAchieved      int                      `gorm:"column:total_point_achieved" json:"total_point_achieved,omitempty"`
-	Status                  string                   `gorm:"column:status" json:"status,omitempty"`
-	CreatedBy               string                   `gorm:"column:created_by" json:"created_by,omitempty"`
-	UpdatedBy               string                   `gorm:"column:updated_by" json:"updated_by,omitempty"`
-	UpdatedAt               time.Time                `gorm:"column:updated_at" json:"updated_at,omitempty"`
-	Achievements            []models.UserAchievement `gorm:"foreignKey:UserID;references:UserID" json:"achievements,omitempty"`
-	Ranks                   []models.UserRank        `gorm:"foreignKey:UserID;references:UserID" json:"ranks,omitempty"`
-	TitleSkills             []models.UserTitleSkill  `gorm:"foreignKey:UserID;references:UserID" json:"title_skills,omitempty"`
+	UserID                  uuid.UUID                  `gorm:"column:user_id" json:"id,omitempty"`
+	FirstName               string                     `gorm:"column:first_name" json:"first_name,omitempty"`
+	LastName                string                     `gorm:"column:last_name" json:"last_name,omitempty"`
+	Email                   string                     `gorm:"column:email" json:"email"`
+	PhoneNumber             string                     `gorm:"column:phone_number" json:"phone_number,omitempty"`
+	PhotoProfile            string                     `gorm:"column:photo_profile_link" json:"photo_profile_link,omitempty"`
+	TitleProfile            string                     `gorm:"column:title_desc_profile" json:"title_desc_profile,omitempty"`
+	Description             string                     `gorm:"column:description" json:"description,omitempty"`
+	EmailVerified           bool                       `gorm:"column:email_verified" json:"email_verified,omitempty"`
+	PhoneVerified           bool                       `gorm:"column:phone_verified" json:"phone_verified,omitempty"`
+	Username                string                     `gorm:"column:username" json:"username"`
+	Gender                  string                     `gorm:"column:gender" json:"gender,omitempty"`
+	TotalCoursesFinished    int                        `gorm:"column:total_courses_finished" json:"total_courses_finished,omitempty"`
+	TotalSubcoursesFinished int                        `gorm:"column:total_subcourses_finished" json:"total_subcourses_finished,omitempty"`
+	TotalPointAchieved      int                        `gorm:"column:total_point_achieved" json:"total_point_achieved,omitempty"`
+	Status                  string                     `gorm:"column:status" json:"status,omitempty"`
+	CreatedBy               string                     `gorm:"column:created_by" json:"created_by,omitempty"`
+	UpdatedBy               string                     `gorm:"column:updated_by" json:"updated_by,omitempty"`
+	UpdatedAt               time.Time                  `gorm:"column:updated_at" json:"updated_at,omitempty"`
+	Achievements            []models.UserAchievement   `gorm:"foreignKey:UserID;references:UserID" json:"achievements,omitempty"`
+	Ranks                   []models.UserRank          `gorm:"foreignKey:UserID;references:UserID" json:"ranks,omitempty"`
+	TitleSkills             []models.UserTitleSkill    `gorm:"foreignKey:UserID;references:UserID" json:"title_skills,omitempty"`
+	QuizResults             []quiz.QuizResult          `gorm:"foreignKey:UserID;references:UserID" json:"quiz_results,omitempty"`
+	CourseEnrollment        []course.EnrollmentModel   `gorm:"foreignKey:UserID;references:UserID" json:"course_enrollment,omitempty"`
+	SubcourseProgress       []course.SubcourseProgress `gorm:"foreignKey:UserID;references:UserID" json:"subcourse_progress,omitempty"`
 }
 
 func (user *UserModel) TableName() string {
